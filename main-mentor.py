0c3eb05dd3912bb923353b9cb8804713dd906836
saman.aghazadeh <saman.aghazadeh@gmail.com>
2017-05-06 16:57:12 -0400

    Ragini::changed file to add pruning

--- main-mentor.py
+++ main-mentor.py
@@ -97,6 +97,64 @@ def get_variables_to_restore(variables_to_restore):
         variables_to_restore.append([v for v in tf.global_variables() if v.name == "fc2/weights:0"][0])
         variables_to_restore.append([v for v in tf.global_variables() if v.name == "fc2/biases:0"][0])
 
+        variables_to_restore.append([v for v in tf.global_variables() if v.name == "fc3/weights:0"][0])
+        variables_to_restore.append([v for v in tf.global_variables() if v.name == "fc3/biases:0"][0])
+
+        return variables_to_restore
+
+"""
+def weights_to_be_pruned():
+
+        
+        ### dictionary to store weights and biases that need to be pruned.
+        weights_prune = {        
+                "w_conv1_1":[v for v in tf.global_variables() if v.name == "conv1_1/weights:0"][0],
+                "b_conv1_1":[v for v in tf.gloval_variables() if v.name == "conv1_1/biases:0"[0],
+                "w_conv1_2":[v for v in tf.global_variables() if v.name =="conv1_2/weights:0"][0],
+                "b_conv1_2":[v for v in tf.global_variables() if v.name == "conv1_2/biases:0"][0],
+                "w_conv2_1":[v for v in tf.global_variables() if v.name == "conv2_1/weights:0"][0],
+                "b_conv2_1":[v for v in tf.global_variables() if v.name == "conv2_1/biases:0"][0],
+                "w_conv2_2":[v for v in tf.global_variables() if v.name == "conv2_2/weights:0"][0],
+                "b_conv2_2":[v for v in tf.global_variables() if v.name == "conv2_2/biases:0"][0],
+                "w_conv3_1":[v for v in tf.global_variables() if v.name == "conv3_1/weights:0"][0],
+                "b_conv3_1":[v for v in tf.global_variables() if v.name == "conv3_1/biases:0"][0],
+                "w_conv3_2":[v for v in tf.global_variables() if v.name == "conv3_2/weights:0"][0],
+                "b_conv3_2":[v for v in tf.global_variables() if v.name == "conv3_2/biases:0"][0],
+                "w_conv3_3":[v for v in tf.global_variables() if v.name == "conv3_3/weights:0"][0],
+                "b_conv3_3":[v for v in tf.global_variables() if v.name == "conv3_3/biases:0"][0],
+                "w_conv4_1":[v for v in tf.global_variables() if v.name == "conv4_1/weights:0"][0],
+                "b_conv4_1":[v for v in tf.global_variables() if v.name == "conv4_1/biases:0"][0],
+                "w_conv4_2":[v for v in tf.global_variables() if v.name == "conv4_2/weights:0"][0],
+                "b_conv4_2":[v for v in tf.global_variables() if v.name == "conv4_2/biases:0"][0],
+                "w_conv4_3":[v for v in tf.global_variables() if v.name == "conv4_3/weights:0"][0],
+                "b_conv4_3":[v for v in tf.global_variables() if v.name == "conv4_3/biases:0"][0],
+                "w_conv5_1":[v for v in tf.global_variables() if v.name == "conv5_1/weights:0"][0],
+                "b_conv5_1":[v for v in tf.global_variables() if v.anme == "conv5_1/biases:0"][0],
+                "w_conv5_2":[v for v in tf.global_variables() if v.name == "conv5_2/weights:0"][0],
+                "b_conv5_2":[v for v in tf.global_variables() if v.name == "conv5_2/biases:0"][0],
+                "w_conv5_3":[v for v in tf.global_variables() if v.name == "conv5_3/weights:0"][0],
+                "b_conv5_3":[v for v in tf.global_variables() if v.name == "conv5_3/biases:0"][0],
+                "w_fc1":[v for v in tf.global_variables() if v.name == "fc1/weights:0"][0],
+                "b_fc1":[v for v in tf.global_variables() if v.name == "fc1/biases:0"][0],
+                "w_fc2":[v for v in tf.global_variables() if v.name == "fc2/weights:0"][0],
+                "b_fc2":[v for v in tf.global_variables() if v.name == "fc2/biases:0"][0]
+                        }
+
+        return weights_prune
+"""
+"""
+def prune_weights(weights_prune, sess,threshold = 0.005):
+        for v in weights_prune:
+            
+            #print(sess.run(v))
+            value = sess.run(v)
+            #pdb.set_trace()
+            under_threshold = abs(value) < threshold 
+            #pdb.set_trace()
+            value[under_threshold] =0
+            
+"""
+        
 def main():
 
 	with tf.Graph().as_default():
@@ -113,8 +171,8 @@ def main():
                 train_last_layer_variables = vgg16.train_last_layer_variables(train_last_layer_variables)
 
 		summary = tf.summary.merge_all()
-		#saver = tf.train.Saver(variables_to_restore)
 		saver = tf.train.Saver()
+		#saver = tf.train.Saver(variables_to_restore)
 		sess = tf.Session()
 		summary_writer = tf.summary.FileWriter(SUMMARY_LOG_DIR, sess.graph)
 		coord = tf.train.Coordinator()
@@ -127,7 +185,16 @@ def main():
 
                 #init = tf.global_variables_initializer()
 		sess.run(init)
-                #saver.restore(sess, "./summary-log/model.ckpt-4999")
+                ## restore weights from summary-log-2 which is base weights model
+                #saver.restore(sess, "./summary-log-2/model.ckpt-3060")
+                #weights_prune =  weights_to_be_pruned()
+
+   #             value1 = [v for v in tf.global_variables() if v.name == "conv1_1/weights:0"][0]
+    #            prune_weights(variables_to_restore, sess)
+     #           value = [v for v in tf.global_variables() if v.name == "conv1_1/weights:0"][0]
+      #          print(sess.run(value1))
+       #         print(sess.run(value))
+        #        pdb.set_trace()
 		eval_correct = evaluation(vgg16.fc3l, labels_placeholder)
 		try:
 			for i in range(NUM_ITERATIONS):
